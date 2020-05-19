abd1e4128ba249160d3aa6a8f2083021040b4bf7
Anggelo Urso <anggelo.urso@gmail.com>
2020-05-18 21:48:01 -0400

    Este es un cambio del script

--- Ejemplos/Python/ejemploInput.py
+++ Ejemplos/Python/ejemploInput.py
@@ -4,3 +4,8 @@ apellido = input(f"Hola {nombre}, cuál es su apellido?: ")
 print(f"Bienvenido {nombre} {apellido}, es un gusto conocerte\n\n")
 
 edad = int(input("Cuál es tu edad: "))
+
+if edad > 18: 
+    print("Wow eres mayor de edad\n")
+else:
+    print("Owww... eres pequeñito\n")
