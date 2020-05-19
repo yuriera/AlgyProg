0c3bcf7e0b9314f94426931af6f664ce091e12ae
Anggelo Urso <anggelo.urso@gmail.com>
2020-05-18 21:42:17 -0400

    Primera versión de ejemplos de algoritmos y programación

---
+++ Ejemplos/Python/ejemploInput.py
@@ -0,0 +1,6 @@
+nombre = input("Ingrese su nombre: ")
+apellido = input(f"Hola {nombre}, cuál es su apellido?: ")
+
+print(f"Bienvenido {nombre} {apellido}, es un gusto conocerte\n\n")
+
+edad = int(input("Cuál es tu edad: "))
---
+++ Ejemplos/Python/ejemploSalto.py
@@ -0,0 +1,3 @@
+print("Esta es una frase con salto de línea \n")
+print("\tEsta es una frase tabulada")
+print("Frase con retorno de carro\r")
