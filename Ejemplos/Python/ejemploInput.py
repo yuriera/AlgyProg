0fd6fd3b9a7d2239b5cc63865ace30ec622a340c
Anggelo Urso <anggelo.urso@gmail.com>
2020-05-19 20:04:39 -0400

    cambios anggelo

--- Ejemplos/Python/ejemploInput.py
+++ Ejemplos/Python/ejemploInput.py
@@ -5,7 +5,7 @@ print(f"Bienvenido {nombre} {apellido}, es un gusto conocerte\n\n")
 
 edad = int(input("Cuál es tu edad: "))
 
-if edad > 18: 
+if edad != 18: 
     print("Wow eres mayor de edad\n")
 else:
     print("Owww... eres pequeñito\n")
