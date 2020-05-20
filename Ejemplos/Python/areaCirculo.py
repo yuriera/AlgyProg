99f91e8875951d859468935f2a58eaca700f011b
Anggelo Urso <anggelo.urso@gmail.com>
2020-05-19 20:20:53 -0400

    Cambio en el cálculo del área

--- Ejemplos/Python/areaCirculo.py
+++ Ejemplos/Python/areaCirculo.py
@@ -1,6 +1,6 @@
 import math
 
 radio = int(input("Ingrese el radio de la circunferencia: "))
-area = math.pi * radio ** 2
+area = math.pi * radio ** 3
 
 print(f"El área de la circunferencia es: {area}")
